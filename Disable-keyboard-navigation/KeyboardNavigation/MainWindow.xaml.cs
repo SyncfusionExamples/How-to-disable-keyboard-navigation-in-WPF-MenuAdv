88bf2a4743f94aa445fea4d0f58c8222ce98ab31
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-10-22 17:47:35 +0530

    Added sample for disable keyboard navigation

---
+++ Disable-keyboard-navigation/KeyboardNavigation/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuAdv_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    e.Handled = true;
+                    break;
+                case Key.Right:
+                    e.Handled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
